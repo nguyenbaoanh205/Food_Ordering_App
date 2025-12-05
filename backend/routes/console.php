6a83f573fd183a6e45aaf381f4975388dc719462
nguyenbaoanh2005 <anhnnbph50226@gmail.com>
2025-12-05 18:42:42 +0700

    comment console.php

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -3,8 +3,8 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote');
 
 Schedule::command('orders:auto-complete')->everyMinute();
